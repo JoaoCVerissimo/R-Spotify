d371c0ee1ce9e98bfbf07ab3645ded4fd4253cb8
JVerissimo <j.verissimo@runtime-revolution.com>
2021-12-17 13:16:20 +0000

    teste

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app";
+import App from "./app.js";
 
 import "./index.css";
 
 ReactDOM.render(
-<App />,
+  <App />,
   document.getElementById("root")
 );
